ef60c7948e8ae85f5f47bbf5e2ad235496db6fca
EvandroLucas <lucasfigteixeira@gmail.com>
2020-10-15 16:46:55 -0300

    Commit 5

--- Customer.java
+++ Customer.java
@@ -23,10 +23,7 @@ public class Customer {
      Enumeration<Rental> rentals = _rentals.elements();
      String result = "Rental Record for " + getName() + "\n";
      while (rentals.hasMoreElements()) {
-        double thisAmount = 0;
         Rental each = (Rental) rentals.nextElement();
-
-        thisAmount = each.getCharge();
 
         // add frequent renter points
         frequentRenterPoints ++;
@@ -36,10 +33,11 @@ public class Customer {
 
         //show figures for this rental
         result += "\t" + each.getMovie().getTitle()+ "\t" +
-            String.valueOf(thisAmount) + "\n";
-        totalAmount += thisAmount;
+            String.valueOf(each.getCharge()) + "\n";
+        totalAmount += each.getCharge();
 
      }
+     
      //add footer lines
      result +=  "Amount owed is " + String.valueOf(totalAmount) + "\n";
      result += "You earned " + String.valueOf(frequentRenterPoints) +
